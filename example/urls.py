301053de037d92cf6bec8061e4908a422db12fa5
MarcoAntonioRovida <marcoantoniorovida@gmail.com>
2021-10-14 19:52:39 -0300

    Prepared the app for static files

--- example/urls.py
+++ example/urls.py
@@ -3,4 +3,5 @@ from example import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("example/<name>", views.hello_there, name="hello_there"),
 ]
--- example/views.py
+++ example/views.py
@@ -1,4 +1,17 @@
+import re
+from django.shortcuts import render
+from django.utils.timezone import datetime
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'example/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
