b70122bd2788ea8aa027215971b808f958372e56
MarcoAntonioRovida <marcoantoniorovida@gmail.com>
2021-10-14 23:35:24 -0300

    Integration with Mongo DB Atlas

--- web_platform/settings.py
+++ web_platform/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'web_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        "CLIENT": {
+           "name": 'InitialDatabase',
+           "host": 'mongodb+srv://InitialUser:MongoPassword@djangomongocluster.mvvci.mongodb.net/InitialDatabase?retryWrites=true&w=majority',
+           "username": 'InitialUser',
+           "password": 'MongoPassword',
+           "authMechanism": "SCRAM-SHA-1",
+        }, 
     }
 }
 
